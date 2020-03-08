d478f121bc5b7652c1208719fe1179d6513ebbc7
Jason <jason.huang.2@stonybrook.edu>
2020-03-08 11:21:01 -0400

    Making a separate branch for testing offline

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,5 +1,11 @@
-try{
-    document.getElementById("menu-user").innerHTML = "Welcome " + JSON.parse(localStorage.getItem('user'))['name'];
-} catch (e) {
-    document.getElementById("menu-user").innerHTML = e.message;
+
+function loadMenu() {
+    // Grabbing name / local data needs to be changed to firebase implementation
+    try {
+        document.getElementById("menu-user").innerHTML = "Welcome " + JSON.parse(localStorage.getItem('user'))['name'];
+    } catch (e) {
+        document.getElementById("menu-user").innerHTML = e.message;
+    }
+
 }
+
